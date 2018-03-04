22bb0613cb1c8e8209786c5b9956e0b394d31db0
Jimit Dholakia <jimitd101@gmail.com>
2018-03-04 11:17:11 +0530

    Separate footer in different file

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+echo '<div class="line"></div>
+            <footer>
+
+                <ul class="nav">
+
+                    <li class="nav-item">
+                        <a href="https://github.com/jimit105" target="_blank" class="nav-link"><i class="fa fa-github fa-2x" title="GitHub"></i></a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="https://in.linkedin.com/in/jimit105" target="_blank" class="nav-link"><i class="fa fa-linkedin fa-2x" title="LinkedIn"></i></a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="https://www.facebook.com/jimit105" target="_blank" class="nav-link"><i class="fa fa-facebook fa-2x" title="Facebook"></i></a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="https://plus.google.com/+JimitDholakia" target="_blank" class="nav-link"><i class="fa fa-google-plus fa-2x" title="Google Plus"></i></a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="https://twitter.com/jimit105" target="_blank" class="nav-link"><i class="fa fa-twitter fa-2x" title="Twitter"></i></a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="https://www.instagram.com/jimit105" target="_blank" class="nav-link"><i class="fa fa-instagram fa-2x" title="Instagram"></i></a>
+                    </li>
+
+                </ul>
+
+            </footer>';
+?>
