9d9f5a48026a3d0f56935aabf3fc3cfe10a05b2c
Jimit Dholakia <jimitd101@gmail.com>
2018-03-11 14:12:52 +0530

    Add Google Translate

--- sidebar.php
+++ sidebar.php
@@ -71,7 +71,24 @@ echo'
 				<i class="fa fa-share"></i>Share an article
 			</a>
 		</li>
+        <li>
+			<a href="#" class="subscribe">
+				<div id="google_translate_element"></div>
+			</a>
+		</li>
 	</ul>
 </nav>';
+
+
+//  Google Translate
+echo '
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: "en"}, "google_translate_element");
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+';
 
 ?>
