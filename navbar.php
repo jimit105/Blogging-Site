e3d08f0a543816a050ecc576b9efefecef201b4c
Jimit Dholakia <jimitd101@gmail.com>
2018-03-13 21:20:12 +0530

    Change name and login/logout depending on login status

--- navbar.php
+++ navbar.php
@@ -17,8 +17,28 @@ echo '
 
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav ml-auto">
-                            <li class="nav-item"><a href="#" class="nav-link">User</a></li>
-                            <li class="nav-item"><a href="#" class="nav-link">Logout</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">';
+                            
+                            
+                            if(isset($_SESSION['username'])){
+echo $_SESSION['username'];
+}else{
+echo 'Guest';
+}
+                            
+                            echo '
+                            </a></li>
+                            <li class="nav-item">';
+                            
+                            
+                            if(isset($_SESSION['username'])){
+echo '<a class="nav-link" href="destroy.php" style="text-decoration:none">Logout</a>';
+}else{
+echo '<a class="nav-link" href="adminlog.php" style="text-decoration:none">Login</a>';
+}
+                            
+                            echo'
+                            </li>
                         </ul>
                     </div>
                 </div>
